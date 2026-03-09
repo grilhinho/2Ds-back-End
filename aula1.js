49eaec04bcb216898f2f83bf6baccd28919c4db4
𝐧𝐢𝐪𝐮𝐞 ☥🦇 ݁ <114404292+grilhinho@users.noreply.github.com>
2026-03-09 08:54:33 -0300

    Add files via upload

---
+++ aula1.js
@@ -0,0 +1,8 @@
+ let nome =  "monique"
+ let altura = 1.61  
+ let idade = 15 
+ let pele = "branca"
+ let estilo = "alternativo"
+ let cabelo = " vermelho, grande, liso com mechas rosas"
+  
+ console.log("Olá, meu nome é " + nome + " ! possuo "+ idade + " anos !  tenho " + altura + " Meu estilo é " + estilo + " ,E meu cabelo é" + cabelo ) 
