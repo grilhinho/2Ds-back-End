b5fc129eaa596505632224cd09ea8ab845806587
𝐧𝐢𝐪𝐮𝐞 ☥🦇 ݁ <114404292+grilhinho@users.noreply.github.com>
2026-08-17 08:58:58 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,18 +1,29 @@
-//npm init 
-// npm express
-// instalar extensão RapidAPI Client no vs code
+// No terminal: 
+// npm init
+// npm i express
+// node index.js -> executa a API
+// instalar extensão RapidAPI Client no VSCode
 const express = require("express")
-const app = express() 
-const port = 3001
+const app = express()
+const port = 3000
 app.use(express.json())
+const fs = require('fs')
 
-app.get("/nome", (req, res) => {
-    res.json({nome: "monique", 
-              idade: "15"
-    })
-})
-
-// ultima parte
- app.listen(port, () => {
-    console.log("API rodando naporta" + port )
+app.post("/aulas", (req, res) => {
+    const aula = req.body
+    try {
+        // abrir o arquivo
+        const aulas = JSON.parse(fs.readFileSync("aulas.json", "utf8"))
+        // adicionar o cliente
+        aulas.push(aula)
+        // salvar o arquivo
+        fs.writeFileSync("aulas.json", JSON.stringify(aulas), "utf8")
+        // resposta
+        res.status(201).json({resposta: "Aula cadastrada!"})
+    } catch (erro) {
+        res.status(500).json({erro: erro.message})
+    }
 }) 
+app.listen(port, ()=>{
+    console.log("API rodando na porta" + port)
+})
