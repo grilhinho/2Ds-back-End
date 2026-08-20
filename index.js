3a8df69b6e6be59482af6c33c437a6700ada5fbf
𝐧𝐢𝐪𝐮𝐞 ☥🦇 ݁ <114404292+grilhinho@users.noreply.github.com>
2026-08-20 11:42:53 -0300

    Update index.js

--- index.js
+++ index.js
@@ -9,21 +9,68 @@ const port = 3000
 app.use(express.json())
 const fs = require('fs')
 
+
+const arquivoID = JSON.parse(fs.readFileSync("id.json", "utf8"))
+let id = arquivoID.id
+
+function atualizarID() {
+    id = id + 1
+    fs.writeFileSync("id.json", JSON.stringify({id: id}), "utf8")
+}
+
+
 app.post("/aulas", (req, res) => {
     const aula = req.body
     try {
-        // abrir o arquivo
         const aulas = JSON.parse(fs.readFileSync("aulas.json", "utf8"))
-        // adicionar o cliente
         aulas.push(aula)
-        // salvar o arquivo
         fs.writeFileSync("aulas.json", JSON.stringify(aulas), "utf8")
-        // resposta
         res.status(201).json({resposta: "Aula cadastrada!"})
     } catch (erro) {
         res.status(500).json({erro: erro.message})
     }
-}) 
+})
+
+app.get("/aulas", (req, res) => {
+    try {
+        const aulas= JSON.parse(fs.readFileSync("aulas.json", "utf8"))
+        res.status(200).json({resposta: aulas})
+    } catch (erro) {
+        res.status(500).json({erro: erro.message})
+    }
+})
+
+
+app.get("/aulas/:id", (req, res) => {
+    const id = req.params.id
+    try {
+        const aulas = JSON.parse(fs.readFileSync("aulas.json", "utf8"))
+        const aula = aulas.find((aula) => aulas.id == id)
+        if(!aulas) {
+            return res.status(404).json({erro: "Aula não existe!"})
+        }
+        res.status(200).json({resposta: aulas})
+    } catch (erro) {
+        res.status(500).json({erro: erro.message})
+    }
+})
+
+app.delete("/aulas/:id", (req, res) => {
+    const id = req.params.id
+    try {
+        const aulas = JSON.parse(fs.readFileSync("aulas.json", "utf8"))
+        const indiceAula = aulas.findIndex((aulas) => aulas.id == id)
+        if (indiceAula == -1) {
+            return res.status(404).json({erro: "A aula não existe"})
+        }
+        aulas.splice(indiceAula, 1)
+        fs.writeFileSync("aulas.json", JSON.stringify(aulas), "utf8")
+        res.status(200).json({resposta: "aula excluída com sucesso!"})
+    } catch (error){
+        res.status(500).json({erro: error.message})
+    }
+})
+
 app.listen(port, ()=>{
     console.log("API rodando na porta" + port)
 })
